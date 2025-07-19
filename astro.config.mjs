35385343d05d9bf816af2741920c5b4a2c7726cb
Jax <lucidbrawler@hatchet.com>
2025-07-19 14:32:31 -0400

    pls update astro config file needed output server line

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [react(), tailwind(), mdx()],
   adapter: netlify()
 });
