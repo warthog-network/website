99c310889c04aebcd4930ef8115ff32b0cd0e5c1
Jax <lucidbrawler@hatchet.com>
2025-07-20 16:58:28 -0400

    added my new https node with cors enabled in the headers

--- src/components/wallet.jsx
+++ src/components/wallet.jsx
@@ -8,6 +8,7 @@ import './Wallet.css';
 const API_URL = '/api/proxy';
 
 const defaultNodeList = [
+  'https://warthognode.duckdns.org',
   'http://51.75.21.134:3001',
   'http://62.72.44.89:3001',
   'http://dev.node-s.com:3001',
