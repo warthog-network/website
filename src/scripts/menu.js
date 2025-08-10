e332f731bc8fb0f225dc1eb0c66ab37163f1eff0
Jax <lucidbrawler@hatchet.com>
2025-08-10 12:48:06 -0400

    some mobile menu changes

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -4,3 +4,7 @@ document.querySelector('.hamburger').addEventListener('click', () => {
   //document.querySelector('.nav-links').classList.toggle('expanded'); 
   document.querySelector('.mobileMenu').classList.toggle('show');
 });
+document.querySelector('.close-menu').addEventListener('click', () => {
+  //document.querySelector('.nav-links').classList.toggle('expanded'); 
+  document.querySelector('.mobileMenu').classList.toggle('hide');
+});
