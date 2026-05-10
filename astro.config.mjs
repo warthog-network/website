c02dd298d81306d9fe6f00fd36e9dc2118f387e9
CoinFu Master Shifu <145641460+CoinFuMasterShifu@users.noreply.github.com>
2026-05-10 18:43:22 +0200

    fix CSP policy

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,7 @@ experimental: {
   csp: {
     directives: [
       "default-src 'self' https://api.coingecko.com",
-      "connect-src 'self' ws: wss: https://api.coingecko.com http://localhost:3000",
+      "connect-src 'self' ws: wss: https://api.coingecko.com http://localhost:3000 https://data.warthog.network",
       "font-src 'self' data: https://fonts.gstatic.com https://fonts.googleapis.com",
       "img-src 'self' data:",
       "object-src 'self'",
